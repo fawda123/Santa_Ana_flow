da6d1dc9d3f6d32220c91359fbfa0dfcc5469278
fawda123 <mbafs2012@gmail.com>
2017-10-04 17:54:51 -0700

    summarized data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -6,16 +6,41 @@ library(tidyverse)
 shds <- c('San Gabriel', 'Upper Santa Ana', 'Middle Santa Ana', 'Lower Santa Ana', 'San Jacinto')
 prstr <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
 
-refs <- readOGR('ignore/Ref_StudyArea_100317.shp')%>% 
-  spTransform(CRS(prstr))
-ants <- readOGR('ignore/Anthro_StudyArea_100317.shp') %>% 
-  spTransform(CRS(prstr))
+# study area sheds, SMC from abv
 shed <- readOGR('ignore/StudyArea_Sheds.shp')%>% 
   spTransform(CRS(prstr)) %>%  
   subset(SMC_Name %in% shds)
 
-# intersect the shed names with refs/ants
+# reference
+refs <- readOGR('ignore/Ref_StudyArea_100317.shp')%>% 
+  spTransform(CRS(prstr)) %>% 
+  raster::intersect(shed) %>% 
+  data.frame %>% 
+  dplyr::select(COMID, SMC_Name, Shape_Leng, matches('_M$|_ME$')) %>% 
+  gather('var', 'flo', -COMID, -SMC_Name, -Shape_Leng) %>% 
+  mutate(var = gsub('_M$|_Me$', '', var)) %>% 
+  separate(var, c('mo', 'clim'), sep = '_')
 
-save(refs, file = 'data/refs.RData', compress = 'xz')
-save(ants, file = 'data/ants.RData', compress = 'xz')
+# anthropogenic
+ants <- readOGR('ignore/Anthro_StudyArea_100317.shp') %>% 
+  spTransform(CRS(prstr)) %>% 
+  data.frame %>% 
+  dplyr::select(COMID, matches('Wet|Dry|Norm')) %>%
+  gather('var', 'val', -COMID) %>% 
+  mutate(
+    var = gsub('_I$', '_In', var),
+    var = gsub('_D$', '_Di', var)
+    ) %>% 
+  separate(var, c('mo', 'clim', 'prob'), sep = '_') %>% 
+  spread('prob', 'val') %>% 
+  mutate(
+    chk = Di + In
+  ) %>% 
+  filter(chk >  0) %>% 
+  dplyr::select(-In, -chk)
+
+# combin the two
+flos <- inner_join(refs, ants, by = c('COMID', 'mo', 'clim'))
+
+save(flos, file = 'data/flos.RData', compress = 'xz')
 save(shed, file = 'data/shed.RData', compress = 'xz')
