b1dab297c8a3d005b74f3443cb8d93884c91ef81
fawda123 <mbafs2012@gmail.com>
2017-10-13 16:29:46 -0700

    added docs folder, created appendix from flo summaries, exec_summ skeleton

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -44,3 +44,54 @@ flos <- inner_join(refs, ants, by = c('COMID', 'mo', 'clim'))
 
 save(flos, file = 'data/flos.RData', compress = 'xz')
 save(shed, file = 'data/shed.RData', compress = 'xz')
+
+######
+# summary data 
+
+# load data
+data(shed)
+data(flos)
+
+# catchment stream length
+lns <- flos %>% 
+  select(SMC_Name, COMID, Shape_Leng) %>% 
+  unique %>% 
+  group_by(SMC_Name) %>% 
+  summarise(totlen = sum(Shape_Leng))
+
+# splits
+flcts <- c(1, 10, 100)
+dicts <- c(0.1, 0.9)
+flos <- flos %>% 
+  mutate(
+    flocat = cut(flo, breaks = c(-Inf, flcts, Inf), labels = c('< 1 cfs', '1 - 10 cfs', '10 - 100 cfs', '> 100 cfs')),
+    dicat = cut(Di, breaks = c(-Inf, dicts, Inf), labels = c('Inflated', 'Stable', 'Diminished'))
+  ) %>% 
+  left_join(lns, by = 'SMC_Name') %>% 
+  mutate(
+    SMC_Name = factor(SMC_Name, 
+                      levels = c('San Gabriel', 'Lower Santa Ana', 'Middle Santa Ana', 'Upper Santa Ana', 'San Jacinto'), 
+                      labels = c('SGB', 'LSA', 'MSA', 'USA', 'SJC')
+    ), 
+    mo = factor(mo, levels = c('Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'))
+  )
+
+
+# summarize by flocat
+sum1 <- flos %>% 
+  group_by(SMC_Name, mo, clim, flocat) %>% 
+  summarise(
+    len = sum(Shape_Leng), 
+    lenper = len / unique(totlen)
+  )
+
+# summarize by dicat
+sum2 <- flos %>% 
+  group_by(SMC_Name, mo, clim, dicat) %>% 
+  summarise(
+    len = sum(Shape_Leng),
+    lenper = len / unique(totlen)
+  )
+
+save(sum1, file = 'data/sum1.RData', compress = 'xz') 
+save(sum2, file = 'data/sum2.RData', compress = 'xz') 
